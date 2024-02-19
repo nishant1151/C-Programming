6f895a3ac1b4db07358499d5a21e691bb0eaf792
Nishant <nishantm.1151@gmail.com>
2024-02-19 12:30:48 +0530

    second commid change in starpattern

--- star_pattern.c
+++ star_pattern.c
@@ -5,9 +5,7 @@
 int main(){
 
 
- int i,j,k,m,n=7;
-char z;
-z="a";
+
     // for(i=1;i<=n;i++){
     //     for(j=1;j<=i;j++){
     //     //     if(j==6 || i==1 && j<6 || i==6 || i>5 && j==1 || i==11 && j>6 || j==11 && i<6){
@@ -22,5 +20,6 @@ z="a";
     //     }
     //     printf("\n");
     // 
+    printf("hello");
             
 }
